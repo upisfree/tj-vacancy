c2ffb6e77938afe685a35b4cad5a3394fa2ecd84
upisfree <upisfree@outlook.com>
2015-06-08 11:03:21 +0600

    Fix featured

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
+function random(min, max) 
+{
+  return Math.floor(Math.random() * (max - min + 1)) + min; 
+}
+
 $(function() 
 {
+  //$('.featured').height($('.featured').height() - 116);
 
+  $('.featured button').click(function()
+  {
+    $('.featured .unvisible').animate({opacity: 1}, 500).removeClass('unvisible');
+    $('.featured button').animate({opacity: 0}, 500).hide(500);
+  });
 });
